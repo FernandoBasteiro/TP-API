49cb5eafc2bfa9723d00c68359f92c8739e96049
Fernando Basteiro <fernando@basteiro.com.ar>
2017-10-04 22:37:44 +0000

    Revert "Update a Publicacion y Subasta."
    This reverts commit b01c209f3874b0209ee221419da0aced13fce349

--- src/modelo/CompraInmediata.java
+++ src/modelo/CompraInmediata.java
@@ -1,18 +1,31 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class CompraInmediata extends Publicacion {
 	private int stock;
 
-	public CompraInmediata(int stock) {
-		super();
+	public CompraInmediata(String nombreDeProducto, String descripcion, ArrayList<String> imagenes, float precioPublicado, int stock) {
+		super(nombreDeProducto, descripcion, imagenes, precioPublicado);
 		this.stock = stock;
 	}
 
 	@Override
 	public int ofertar(float monto, Usuario comprador, String medioDePago) {
 		//TODO Realziar venta.
+		if (this.stock > 0) {
+			if (--this.stock == 0) {
+				this.estadoPublicacion = "Finalizada";
+			}
+		}
 		return 0;
 	}
+
+	@Override
+	public float getPrecioActual() {
+		return this.precioPublicado;
+	}
+	
 	
 	
 }
